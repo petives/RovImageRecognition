5be5b059ece782d042dd46e75910bce1a556bb4d
petives <peterivester@gmail.com>
2019-04-04 21:42:15 -0400

    Add project files.

---
+++ Engucv33Apps/FormShapeDetection.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace RovShapeRecognition
+{
+    partial class FormShapeDetection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.webcamToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.detectShapesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1048, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.webcamToolStripMenuItem,
+            this.detectShapesToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(179, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // webcamToolStripMenuItem
+            // 
+            this.webcamToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.stopToolStripMenuItem});
+            this.webcamToolStripMenuItem.Name = "webcamToolStripMenuItem";
+            this.webcamToolStripMenuItem.Size = new System.Drawing.Size(179, 26);
+            this.webcamToolStripMenuItem.Text = "Webcam";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(115, 26);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(115, 26);
+            this.stopToolStripMenuItem.Text = "Stop";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
+            // 
+            // detectShapesToolStripMenuItem
+            // 
+            this.detectShapesToolStripMenuItem.Name = "detectShapesToolStripMenuItem";
+            this.detectShapesToolStripMenuItem.Size = new System.Drawing.Size(179, 26);
+            this.detectShapesToolStripMenuItem.Text = "Detect Shapes";
+            this.detectShapesToolStripMenuItem.Click += new System.EventHandler(this.detectShapesToolStripMenuItem_Click_1);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(13, 32);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(745, 406);
+            this.panel1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(764, 35);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(129, 36);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Shapes";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(764, 77);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(129, 36);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "Triangles";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(764, 119);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(129, 36);
+            this.textBox5.TabIndex = 6;
+            this.textBox5.Text = "Squares";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox7.Location = new System.Drawing.Point(764, 161);
+            this.textBox7.Multiline = true;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(129, 36);
+            this.textBox7.TabIndex = 8;
+            this.textBox7.Text = "Rectangles";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox9.Location = new System.Drawing.Point(764, 203);
+            this.textBox9.Multiline = true;
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(129, 36);
+            this.textBox9.TabIndex = 10;
+            this.textBox9.Text = "Circles";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(899, 203);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(129, 36);
+            this.textBox2.TabIndex = 15;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(899, 161);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(129, 36);
+            this.textBox4.TabIndex = 14;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.Location = new System.Drawing.Point(899, 119);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(129, 36);
+            this.textBox6.TabIndex = 13;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox8.Location = new System.Drawing.Point(899, 77);
+            this.textBox8.Multiline = true;
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(129, 36);
+            this.textBox8.TabIndex = 12;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox10.Location = new System.Drawing.Point(899, 35);
+            this.textBox10.Multiline = true;
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(129, 36);
+            this.textBox10.TabIndex = 11;
+            this.textBox10.Text = "Number";
+            // 
+            // FormShapeDetection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1048, 457);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormShapeDetection";
+            this.Text = "ShapeDetection";
+            this.Load += new System.EventHandler(this.FormShapeDetection_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem detectShapesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem webcamToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox10;
+    }
+}
+
---
+++ Engucv33Apps/FormShapeDetection.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+
+
+namespace RovShapeRecognition
+{
+    public partial class FormShapeDetection : Form
+    {
+        Image<Bgr, byte> imgInput;
+        public FormShapeDetection()
+        {
+            InitializeComponent();
+        }
+
+        private void FormShapeDetection_Load(object sender, EventArgs e)
+        {
+            panel1.AutoScroll = true;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            try
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    imgInput = new Image<Bgr, byte>(dialog.FileName);
+                    pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+                    pictureBox1.Image = imgInput.Bitmap;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("File format is not image.");
+            }
+        }
+        bool playing = false;
+        VideoCapture capture = new VideoCapture();
+        Mat mCap = new Mat();
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!playing) //just to initialize only once
+            {
+                playing = true;
+                pictureBox1.Width = capture.Width;
+                pictureBox1.Height = capture.Height;
+            }
+            capture.ImageGrabbed += Capture_ImageGrabbed;
+            capture.Start();
+            capture.SetCaptureProperty(Emgu.CV.CvEnum.CapProp.FrameWidth, 640);
+            capture.SetCaptureProperty(Emgu.CV.CvEnum.CapProp.FrameHeight, 540);
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            playing = false;
+            capture.Stop();
+        }
+
+        private void Capture_ImageGrabbed(object sender, EventArgs e)
+        {
+            capture.Read(mCap);
+            try
+            {
+                Image<Bgr, byte> capImage = new Image<Bgr, byte>(mCap.Bitmap);
+                CvInvoke.Flip(capImage, capImage, Emgu.CV.CvEnum.FlipType.Horizontal);
+                findShapes(capImage);
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Something went wrong...\nTry starting the webcam when an image is not already loaded.");
+                Application.Exit();
+            }
+        }
+
+        private void detectShapesToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            if (imgInput == null)
+            {
+                MessageBox.Show("No image detected.");
+                return;
+            }
+            findShapes(imgInput);
+        }
+
+        public int Get(Mat mat, int index)
+        {
+            if (mat.Depth != Emgu.CV.CvEnum.DepthType.Cv32S)
+            {
+                throw new ArgumentOutOfRangeException("ContourData must have Cv32S hierarchy element type.");
+            }
+            if (mat.Rows != 1)
+            {
+                throw new ArgumentOutOfRangeException("ContourData must have one hierarchy hierarchy row.");
+            }
+            if (mat.NumberOfChannels != 4)
+            {
+                throw new ArgumentOutOfRangeException("ContourData must have four hierarchy channels.");
+            }
+            if (mat.Dims != 2)
+            {
+                throw new ArgumentOutOfRangeException("ContourData must have two dimensional hierarchy.");
+            }
+            long elementStride = mat.ElementSize / sizeof(Int32);
+            var offset = (long)mat + index * elementStride;
+            if (0 <= offset && offset < Hierarchy.Total.ToInt64() * elementStride)
+            {
+                unsafe
+                {
+                    return *((Int32*)Hierarchy.DataPointer.ToPointer() + offset);
+                }
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        private void findShapes(Image<Bgr, byte> imgInput)
+        {
+            Image<Gray, byte> imgCanny = new Image<Gray, byte>(imgInput.Width, imgInput.Height, new Gray(0));
+            imgCanny = imgInput.Canny(20, 50);
+
+            //pictureBox1.Image = imgCanny.Bitmap;
+
+            VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+            Mat mat = new Mat();
+            CvInvoke.FindContours(imgCanny, contours, mat, Emgu.CV.CvEnum.RetrType.Tree, Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxSimple);
+            
+            int[] shapes = new int[4];
+            /* [0] . . . Triangle
+             * [1] . . . Square
+             * [2] . . . Rectangle
+             * [3] . . . Circle
+             */
+
+            for (int i = 0; i < contours.Size; i++)
+            {
+                double perimeter = CvInvoke.ArcLength(contours[i], true);
+                VectorOfPoint approx = new VectorOfPoint();
+                CvInvoke.ApproxPolyDP(contours[i], approx, 0.04 * perimeter, true);
+
+                CvInvoke.DrawContours(imgInput, contours, i, new MCvScalar(0, 0, 255), 2);
+                pictureBox1.Image = imgInput.Bitmap;
+                //moments  center of the shape
+
+                var moments = CvInvoke.Moments(contours[i]);
+                int x = (int)(moments.M10 / moments.M00);
+                int y = (int)(moments.M01 / moments.M00);
+
+                if (CvInvoke.ContourArea(approx) > 250) //ONLY CONSIDERS SHAPES WITH AREA GREATER THAN 100
+                {
+                    if (approx.Size == 3)
+                    {
+                        shapes[0]++;
+                        CvInvoke.PutText(imgInput, "Triangle", new Point(x, y),
+                            Emgu.CV.CvEnum.FontFace.HersheySimplex, 0.5, new MCvScalar(0, 255, 0), 1);
+                    }
+
+                    if (approx.Size == 4)
+                    {
+                        Rectangle rect = CvInvoke.BoundingRectangle(contours[i]);
+
+                        double ar = (double)rect.Width / rect.Height;
+
+                        if (ar >= 0.95 && ar <= 1.05)
+                        {
+                            shapes[1]++;
+                            CvInvoke.PutText(imgInput, "Square", new Point(x, y),
+                            Emgu.CV.CvEnum.FontFace.HersheySimplex, 0.5, new MCvScalar(0, 255, 0), 1);
+                        }
+                        else
+                        {
+                            shapes[2]++;
+                            CvInvoke.PutText(imgInput, "Rectangle", new Point(x, y),
+                            Emgu.CV.CvEnum.FontFace.HersheySimplex, 0.5, new MCvScalar(0, 255, 0), 1);
+                        }
+
+                    }
+
+                    if (approx.Size > 6)
+                    {
+                        CvInvoke.PutText(imgInput, "Circle", new Point(x, y),
+                        Emgu.CV.CvEnum.FontFace.HersheySimplex, 0.5, new MCvScalar(0, 255, 0), 1);
+                        shapes[3]++;
+                    }
+
+                    pictureBox1.Image = imgInput.Bitmap;
+                }
+            }
+            textBox8.Text = shapes[0].ToString();
+            textBox6.Text = shapes[1].ToString();
+            textBox4.Text = shapes[2].ToString();
+            textBox2.Text = shapes[3].ToString();
+        }
+    }
+}
+
+
